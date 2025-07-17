e3c3e602b9d8e593c4c091e86bd5f0d094877694
Cesar Outl <cesar71.oliveira@outlook.com>
2025-07-16 23:56:46 -0300

    Salvando main atual

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+# Comandos Print
+
 print("Bora primeira aula")
+print(3)
+print(3 + 4)
+print("Olá" + " " + "Turma")
+
+# Inputs
+
+input("Digite seu nome: ")
+
+# Print com Input
+
+print(input("Digite seu nome: "))
+
+print("Olá, " + input("Digite seu nome: ") + "!")
+
+# Programa pra retornar números de caracteres
+
+print(len(input("Digite o seu nome: ")))
+
+# Programa para solicitar e somar dois valores
+
+print(input("Digite um valor: ") + input("Digite outro valor: "))
+
+# Fazendo cast, que é definir o tipo do input
+
+print(int(input("Digite um valor: ")) + int(input("Digite outro valor: ")))
+
+# Variáveis
+# int 8
+# float 5.2
+# string "Nome"
+# Boolean True or False
+# Para ver o tipo da variável use o comando type > print(type(nome_variável))
+
